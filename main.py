fd05e7c23fcabb35846d93ce8d87c053c7a4dbb5
Caleb Bubel <masquerade@Calebs-Mac-mini.local>
2023-03-10 13:31:09 -0600

    Updated calculations to show final exam grades for A, B, and C.

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ import tkinter as tk
 
 def calculate():
     participation = float(entry1.get())
-    alc = float(entry2.get())
+    a_l_c = float(entry2.get())
     midterm_exam = float(entry3.get())
 
-    overall_average = participation + alc + midterm_exam
+    overall_average = participation + a_l_c + midterm_exam
 
-    if overall_average > 1350:
-        label4.configure(text="You don't even have to take the final exam. Congrats!")
-    else:
-        final_score_needed = (1350 - overall_average)
-        label4.configure(
-            text="You need to score " + str(final_score_needed) + " points on the final to pass the class.")
-        label5.configure(
-            text=" That is equivalent to a " + str(round((final_score_needed / 600) * 100, 2)) + "% on the final.")
+    points_needed_for_A = 1800 - overall_average
+    points_needed_for_B = 1600 - overall_average
+    points_needed_for_C = 1400 - overall_average
+
+    display_points(points_needed_for_A, points_needed_for_B, points_needed_for_C)
+
+
+def display_points(points_needed_for_A, points_needed_for_B, points_needed_for_C):
+    percentage_needed_for_A = round((points_needed_for_A / 600) * 100, 1)
+    percentage_needed_for_B = round((points_needed_for_B / 600) * 100, 1)
+    percentage_needed_for_C = round((points_needed_for_C / 600) * 100, 1)
+
+    label6.configure(text="You need " + str(points_needed_for_A) + " more points to get an 'A' in the class."
+                                                                   " That is equivalent to a " + str(
+        percentage_needed_for_A) + "% on the final exam.")
+
+    label7.configure(text="You need " + str(points_needed_for_B) + " more points to get a 'B' in the class."
+                                                                   " That is equivalent to a " + str(
+        percentage_needed_for_B) + "% on the final exam.")
+
+    label8.configure(text="You need " + str(points_needed_for_C) + " more points to get a 'C' in the class."
+                                                                   " That is equivalent to a " + str(
+        percentage_needed_for_C) + "% on the final exam.")
 
 
 root = tk.Tk()
@@ -44,5 +59,14 @@ label4.grid(row=4, column=0)
 
 label5 = tk.Label(root, text="")
 label5.grid(row=5, column=0)
+
+label6 = tk.Label(root, text="")
+label6.grid(row=6, column=0)
+
+label7 = tk.Label(root, text="")
+label7.grid(row=7, column=0)
+
+label8 = tk.Label(root, text="")
+label8.grid(row=8, column=0)
 
 root.mainloop()
